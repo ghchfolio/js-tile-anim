1ca2b63ffba0be49ce104aa2a85dbf47a4a958a2
CH Folio <ghchfolio@gmail.com>
2024-01-12 03:24:55 -0600

    move logic to generateTiles function

--- js/main.js
+++ js/main.js
@@ -2,37 +2,40 @@
 
 (function () {
     const maxTiles = 20;
-    const colors = ['#ff9999', '#ff80ff', '#809fff', '#5cd6d6', '#85e085', '#ffd633', '#66d9ff', '#cc99ff', '#cccccc'];
-    const tiles = document.getElementById('tiles');
-
     let randomTileIndex = 0;
     let lastIndex = 0;
 
-    // dynamically add tiles with diff bg color the tiles element
-    for (let i = 0; i < maxTiles; i++) {
-        const tile = document.createElement('div');
-        const randomColorIndex = Math.floor(Math.random() * colors.length);
-        const randomColor = colors[randomColorIndex];
+    generateTiles();
+    animateTiles();
 
-        tile.classList.add('tile');
-        tile.textContent = i;
-        tile.style.backgroundColor = randomColor;
-        tile.addEventListener('animationend', function (evt) {
-            this.classList.remove('fade');
-            animateTile();
-        });
+    function generateTiles() {
+        const colors = ['#ff9999', '#ff80ff', '#809fff', '#5cd6d6', '#85e085', '#ffd633', '#66d9ff', '#cc99ff', '#cccccc'];
+        const tiles = document.getElementById('tiles');
 
-        tiles.appendChild(tile);
+        for (let i = 0; i < maxTiles; i++) {
+            const tile = document.createElement('div');
+            const randomColorIndex = Math.floor(Math.random() * colors.length);
+            const randomColor = colors[randomColorIndex];
+
+            tile.classList.add('tile');
+            tile.textContent = i;
+            tile.style.backgroundColor = randomColor;
+            tile.addEventListener('animationend', function (evt) {
+                this.classList.remove('fade');
+                animateTiles();
+            });
+
+            tiles.appendChild(tile);
+        }
     }
 
-    animateTile();
-
-    function animateTile() {
+    function animateTiles() {
         let randomTile = null;
+
         randomTileIndex = Math.floor(Math.random() * maxTiles);
 
         if (lastIndex === randomTileIndex) {
-            animateTile();
+            animateTiles();
         } else {
             randomTile = document.getElementsByClassName('tile')[randomTileIndex];
             if (!randomTile.classList.contains('fade')) randomTile.classList.
